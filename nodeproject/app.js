028c5750d746a78c8b9d6c5df83bd7b7b833e54e
Andrea Chinelliott <andrea@attenchin.com>
2017-06-22 17:06:20 +0000

    Express installed

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,17 @@
-var attenchin = "attenchin variable";
-console.log(attenchin);
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+    res.send('Hi there');
+    
+});
+
+app.get('/routing',function(req,res){
+    res.send('Hi there. This is a routing test.');
+    
+});
+
+var port = process.env.PORT;
+app.listen(port,function(err){
+    console.log('This server is running on port:' + port);
+});
